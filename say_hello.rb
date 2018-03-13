b21d634a716ca24c40a9bf4c3441aef81e54ad5d
Anne Edmondson <edmondson.anne@gmail.com>
2018-03-13 01:34:27 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,4 @@
-# Build your say_hello method helper_method
-def say_hello (name="Ruby Programmer")"
+# Build your say_hello method here
+def say_hello (name="Ruby Programmer")
   puts "Hello #{name}!"
 end
